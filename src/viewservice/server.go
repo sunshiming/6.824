1ac147d3eafebc9e8a182b89775d45b6a511e117
miaoever <leo.miao.ever@gmail.com>
2014-12-05 23:39:48 +0800

    fix deadlock bug.

--- src/viewservice/server.go
+++ src/viewservice/server.go
@@ -1,4 +1,3 @@
-
 package viewservice
 
 import "net"
@@ -9,36 +8,51 @@ import "sync"
 import "fmt"
 import "os"
 
+type ServerStat struct {
+	LastPing  time.Time
+	DeadCount int
+	alive     bool
+}
+
 type ViewServer struct {
-  mu sync.Mutex
-  l net.Listener
-  dead bool
-  me string
-
-
-  // Your declarations here.
+	mu          *sync.Mutex
+	l           net.Listener
+	dead        bool
+	me          string
+	view        View
+	state       map[string]*ServerStat
+	IdleServers []string
+	viewnum     uint64
 }
 
 //
 // server Ping RPC handler.
 //
 func (vs *ViewServer) Ping(args *PingArgs, reply *PingReply) error {
+	vs.mu.Lock()
+	//first start
+	if vs.viewnum == 0 {
+		vs.view.Primary = args.Me
+		vs.viewnum++
+	}
 
-  // Your code here.
-
-  return nil
+	if args.Viewnum == 0 {
+		vs.state[args.Me].LastPing = time.Now()
+	} else {
+		vs.state[args.Me].LastPing = time.Now()
+	}
+	reply.View = vs.view
+	vs.mu.Unlock()
+	return nil
 }
 
-// 
+//
 // server Get() RPC handler.
 //
 func (vs *ViewServer) Get(args *GetArgs, reply *GetReply) error {
-
-  // Your code here.
-
-  return nil
+	// Your code here.
+	return nil
 }
-
 
 //
 // tick() is called once per PingInterval; it should notice
@@ -47,7 +61,35 @@ func (vs *ViewServer) Get(args *GetArgs, reply *GetReply) error {
 //
 func (vs *ViewServer) tick() {
 
-  // Your code here.
+	if vs.view.Viewnum == 0 {
+		return
+	}
+
+	Intvl4Primary := time.Since(vs.state[vs.view.Primary].LastPing)
+	Intvl4Backup := time.Since(vs.state[vs.view.Backup].LastPing)
+
+	if Intvl4Primary > PingInterval {
+		vs.state[vs.view.Primary].DeadCount++
+		if vs.state[vs.view.Primary].DeadCount >= DeadPings {
+			vs.state[vs.view.Primary].alive = false
+		}
+	}
+	if Intvl4Backup > PingInterval {
+		vs.state[vs.view.Backup].DeadCount++
+		if vs.state[vs.view.Backup].DeadCount >= DeadPings {
+			vs.state[vs.view.Backup].alive = false
+		}
+	}
+	for idx, v := range vs.IdleServers {
+		if time.Since(vs.state[v].LastPing) > PingInterval {
+			vs.state[v].DeadCount++
+		}
+		if vs.state[v].DeadCount >= DeadPings {
+			delete(vs.state, v)
+			vs.IdleServers = append(vs.IdleServers[:idx], vs.IdleServers[idx+1:]...)
+
+		}
+	}
 }
 
 //
@@ -56,54 +98,57 @@ func (vs *ViewServer) tick() {
 // please don't change this function.
 //
 func (vs *ViewServer) Kill() {
-  vs.dead = true
-  vs.l.Close()
+	vs.dead = true
+	vs.l.Close()
 }
 
 func StartServer(me string) *ViewServer {
-  vs := new(ViewServer)
-  vs.me = me
-  // Your vs.* initializations here.
+	vs := new(ViewServer)
+	vs.me = me
+	vs.state = make(map[string]*ServerStat)
+	vs.IdleServers = make([]string, 10)
+	vs.mu = &sync.Mutex{}
+	vs.view = View{0, "", ""}
 
-  // tell net/rpc about our RPC server and handlers.
-  rpcs := rpc.NewServer()
-  rpcs.Register(vs)
+	// tell net/rpc about our RPC server and handlers.
+	rpcs := rpc.NewServer()
+	rpcs.Register(vs)
 
-  // prepare to receive connections from clients.
-  // change "unix" to "tcp" to use over a network.
-  os.Remove(vs.me) // only needed for "unix"
-  l, e := net.Listen("unix", vs.me);
-  if e != nil {
-    log.Fatal("listen error: ", e);
-  }
-  vs.l = l
+	// prepare to receive connections from clients.
+	// change "unix" to "tcp" to use over a network.
+	os.Remove(vs.me) // only needed for "unix"
+	l, e := net.Listen("unix", vs.me)
+	if e != nil {
+		log.Fatal("listen error: ", e)
+	}
+	vs.l = l
 
-  // please don't change any of the following code,
-  // or do anything to subvert it.
+	// please don't change any of the following code,
+	// or do anything to subvert it.
 
-  // create a thread to accept RPC connections from clients.
-  go func() {
-    for vs.dead == false {
-      conn, err := vs.l.Accept()
-      if err == nil && vs.dead == false {
-        go rpcs.ServeConn(conn)
-      } else if err == nil {
-        conn.Close()
-      }
-      if err != nil && vs.dead == false {
-        fmt.Printf("ViewServer(%v) accept: %v\n", me, err.Error())
-        vs.Kill()
-      }
-    }
-  }()
+	// create a thread to accept RPC connections from clients.
+	go func() {
+		for vs.dead == false {
+			conn, err := vs.l.Accept()
+			if err == nil && vs.dead == false {
+				go rpcs.ServeConn(conn)
+			} else if err == nil {
+				conn.Close()
+			}
+			if err != nil && vs.dead == false {
+				fmt.Printf("ViewServer(%v) accept: %v\n", me, err.Error())
+				vs.Kill()
+			}
+		}
+	}()
 
-  // create a thread to call tick() periodically.
-  go func() {
-    for vs.dead == false {
-      vs.tick()
-      time.Sleep(PingInterval)
-    }
-  }()
+	// create a thread to call tick() periodically.
+	go func() {
+		for vs.dead == false {
+			vs.tick()
+			time.Sleep(PingInterval)
+		}
+	}()
 
-  return vs
+	return vs
 }
